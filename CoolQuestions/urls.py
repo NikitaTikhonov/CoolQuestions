62740adc15b65b2be59abef007ff1161eb7128e3
Nikita Tikhonov <tikhonnikita987@gmail.com>
2019-07-03 13:35:40 +0300

    add view and endpoint for questions

--- CoolQuestions/urls.py
+++ CoolQuestions/urls.py
@@ -41,6 +41,9 @@ urlpatterns = [
     path('api/',
          include("users.api.urls")),
 
+    path('api/',
+         include("questions.api.urls")),
+
     path('api/api-auth/',
          include("rest_auth.urls")),
 
