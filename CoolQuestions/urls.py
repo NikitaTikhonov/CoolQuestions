dbe9547bf5fdef2782296bf0ed325b4f24dad3c1
Nikita Tikhonov <tikhonnikita987@gmail.com>
2019-07-03 10:29:46 +0300

    add index.html file and ulrs for it

--- CoolQuestions/urls.py
+++ CoolQuestions/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django_registration.backends.one_step.views import RegistrationView
 
+from core.views import IndexTemplateView
 from users.forms import CustomUserForm
 
 urlpatterns = [
@@ -40,4 +41,6 @@ urlpatterns = [
          include("rest_auth.urls")),
     path('api/api-auth/registration/',
          include("rest_auth.registration.urls")),
+
+    re_path(r"^.*$", IndexTemplateView.as_view(), name="entry-point")
 ]
