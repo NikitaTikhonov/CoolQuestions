25039d87adbfdd70103370d3996cf24e58ac0b63
Nikita Tikhonov <tikhonnikita987@gmail.com>
2019-07-03 14:48:29 +0300

    add read/update/delete endpoins for answers

--- questions/api/urls.py
+++ questions/api/urls.py
@@ -9,10 +9,13 @@ router.register(r"questions", qv.QuestionViewSet)
 
 urlpatterns=[
     path("", include(router .urls)),
-    path("questions/<slug:slug>/answer/",
-         qv.AnswerCreateAPIView.as_view(),
-         name="create-answer"),
     path("questions/<slug:slug>/answers/",
          qv.AnswerListAPIView.as_view(),
-         name="answer-list")
+         name="answer-list"),
+    path("questions/<slug:slug>/answer/",
+         qv.AnswerCreateAPIView.as_view(),
+         name="answer-create"),
+    path("answers/<int:pk>/",
+         qv.AnswerRUDAPIView.as_view(),
+         name="answer-detail")
 ]
